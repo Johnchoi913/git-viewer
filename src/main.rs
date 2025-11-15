011aa99a7adc4558d1f0a97879400443933dfedc
John Choi <johnchoi913@yahoo.com>
2025-11-15 17:11:21 -0500

    Reverse sort and simplify - Add git2::Sort::REVERSE to print out in proper chronological order - Remove target and target_head and instead use push_head for revwalk

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,10 @@ use git2::{Error, Repository};
 fn main() -> Result<(), Error> {
     let repo = Repository::open(".")?;
 
-    let head = repo.head()?;
-
-    let head_target = head.target().unwrap();
-
     let mut revwalk = repo.revwalk()?;
 
-    revwalk.push(head_target)?;
-    revwalk.set_sorting(git2::Sort::TIME)?;
+    revwalk.push_head()?;
+    revwalk.set_sorting(git2::Sort::TIME | git2::Sort::REVERSE)?;
 
     for node in revwalk {
         let oid = node?;
