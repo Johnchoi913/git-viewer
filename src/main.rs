507b24f965bbd5a9b6d9cdb932e9b794a724def0
John Choi <johnchoi913@yahoo.com>
2025-11-15 17:07:01 -0500

    Revwalk - Add revwalk to walk through commit history

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use git2::{Error, Repository};
 
-fn main() -> Result<(),  Error>{
-
+fn main() -> Result<(), Error> {
     let repo = Repository::open(".")?;
 
     let head = repo.head()?;
 
     let head_target = head.target().unwrap();
 
-    println!("{}", head_target);
+    let mut revwalk = repo.revwalk()?;
+
+    revwalk.push(head_target)?;
+    revwalk.set_sorting(git2::Sort::TIME)?;
+
+    for node in revwalk {
+        let oid = node?;
+        let commit = repo.find_commit(oid)?;
+        println!("{} {}", oid, commit.summary().unwrap_or(""));
+    }
+
     Ok(())
 }
